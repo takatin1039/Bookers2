a8223b19064cf064b34f1ff8bc97fa15708850e7
takatin1039 <kinoshita.takayuki.infratop@gmail.com>
2020-03-31 22:21:26 +0900

    Initial Commit Message

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'index' => 'homes#index'
+  root 'homes#index'
+  get 'home/about', to: 'homes#about'
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
